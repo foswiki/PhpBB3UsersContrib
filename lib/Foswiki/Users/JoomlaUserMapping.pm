4d52bb7c85428fa1b3003eb1248fd15a84cfd0b5
SvenDowideit <SvenDowideit@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2009-03-11 05:18:07 +0000

    Item1243: start porting
    git-svn-id: http://svn.foswiki.org/trunk/JoomlaUsersContrib@2971 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Users/JoomlaUserMapping.pm
+++ lib/Foswiki/Users/JoomlaUserMapping.pm
@@ -1,13 +1,10 @@
 # Module of Foswiki Collaboration Platform, http://Foswiki.org/
 #
-# Copyright (C) 2006, 2007 Sven Dowideit, SvenDowideit@home.org.au
-# and Foswiki Contributors. All Rights Reserved. Foswiki Contributors
-# are listed in the AUTHORS file in the root of this distribution.
-# NOTE: Please extend that file, not this notice.
+# Copyright (C) 2006-9 Sven Dowideit, SvenDowideit@fosiki.com
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
+# as published by the Free Software Foundation; either version 3
 # of the License, or (at your option) any later version. For
 # more details read LICENSE in the root of this distribution.
 #
