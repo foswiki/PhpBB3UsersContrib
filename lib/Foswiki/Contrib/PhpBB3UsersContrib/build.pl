581c4e039cdf037de7a5a7fbb33c4fafca9b8c2a
WillNorris <WillNorris@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2009-03-13 03:35:52 +0000

    Item1244: shrinkage
    git-svn-id: http://svn.foswiki.org/trunk/PhpBB3UsersContrib@3053 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Contrib/PhpBB3UsersContrib/build.pl
+++ lib/Foswiki/Contrib/PhpBB3UsersContrib/build.pl
@@ -6,27 +6,5 @@ BEGIN { unshift @INC, split( /:/, $ENV{FOSWIKI_LIBS} ) }
 
 use Foswiki::Contrib::Build;
 
-# Declare our build package
-package BuildBuild;
-use base qw( Foswiki::Contrib::Build );
-
-sub new {
-    my $class = shift;
-    return bless( $class->SUPER::new( "PhpBB3UsersContrib" ), $class );
-}
-
-# Example: Override the build target
-sub target_build {
-    my $this = shift;
-
-    $this->SUPER::target_build();
-
-    # Do other build stuff here
-}
-
-# Create the build object
-my $build = new BuildBuild();
-
-# Build the target on the command line, or the default target
-$build->build($build->{target});
-
+my $build = new Foswiki::Contrib::Build( 'PhpBB3UsersContrib ' );
+$build->build( $build->{target} );
