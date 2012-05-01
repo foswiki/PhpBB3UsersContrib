ee65c10fdc83f26ad650916aa2fee6782f19afa6
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-01 14:07:26 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule
    git-svn-id: http://svn.foswiki.org/trunk/PhpBB3UsersContrib@14680 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Contrib/PhpBB3UsersContrib/build.pl
+++ lib/Foswiki/Contrib/PhpBB3UsersContrib/build.pl
@@ -6,5 +6,5 @@ BEGIN { unshift @INC, split( /:/, $ENV{FOSWIKI_LIBS} ) }
 
 use Foswiki::Contrib::Build;
 
-my $build = new Foswiki::Contrib::Build( 'PhpBB3UsersContrib' );
+my $build = new Foswiki::Contrib::Build('PhpBB3UsersContrib');
 $build->build( $build->{target} );
